6bfaf979c46be00b3976e202c864840117517e56
0utKast <educaleft@gmail.com>
2016-11-22 20:15:33 +0000

    firebase 2
    Aplicaciones del Videotutorial Nº 2 del Curso de Firebase.

---
+++ app.js
@@ -0,0 +1,37 @@
+(function() {
+
+//Inicializar Firebase
+  const config = {
+	  apiKey: "AIzaSyAkFY4FYBebWtMhLNU_eTk3YyBeybH4SHI",
+    authDomain: "realtime01-22595.firebaseapp.com",
+    databaseURL: "https://realtime01-22595.firebaseio.com",
+    storageBucket: "realtime01-22595.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  //Obtener elementos
+  const preObject = document.getElementById('objeto');
+
+  //Crear Referencias
+  const dbRefObject = firebase.database().ref().child('objeto');
+
+  // Sincronizar cambios objeto
+  dbRefObject.on('value', snap => {
+    preObject.innerText = JSON.stringify(snap.val(),null, 3);
+  });
+
+
+} ());
+
+
+
+
+
+
+
+
+
+
+
+
+
